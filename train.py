58c78fe12861c124b4f374ce683f6535195e32a4
Davy Neven <neven.davy@gmail.com>
2022-10-07 14:31:07 +0200

    modified train

--- train.py
+++ train.py
@@ -2,8 +2,8 @@ from engine import Engine
 from configs.config import get_config
 from ml4vision.client import Client
 
-API_KEY = '8bf9cd1d7a1c130e206a8434a5bc89d080e61ea7'
-PROJECT_NAME = 'Pollen counting (CPD-1)'
+API_KEY = 'API_KEY'
+PROJECT_NAME = 'PROJECT_NAME'
 PROJECT_OWNER = None
 
 # init client
@@ -15,11 +15,10 @@ project_location = project.pull(location='./data')
 
 # get config
 config = get_config(project_location=project_location, categories=project.categories)
-config.solver.max_epochs = 2
 
 # train model
 engine = Engine(config)
 engine.train()
 
 # upload model
-# engine.upload(project)
+engine.upload(project)
