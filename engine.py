e309bb94a1052dc65d65c4a8eb0943b1b32ba7e5
Davy Neven <neven.davy@gmail.com>
2022-11-23 17:52:26 +0100

    new client

--- engine.py
+++ engine.py
@@ -11,6 +11,7 @@ from utils.collate import box_collate_fn
 from utils.parse_detections import parse_detections, parse_batch_detections
 from utils.ap_metrics import APMeter
 from tqdm import tqdm
+from ml4vision.client import MLModel
 
 class Engine:
 
@@ -227,20 +228,21 @@ class Engine:
         traced_model = torch.jit.trace(model, torch.randn(1, 3, config.transform.min_size, config.transform.min_size)) 
         traced_model.save(os.path.join(config.save_location, 'best_ap_model.pt'))
 
-        # upload model
-        remote_model = project.client.get_or_create_model(
-            f"{project.name}-model",
-            description='',
-            project=project.uuid,
-            categories=project.categories,
-            annotation_type="BBOX",
-            architecture="object_detection_fn"
-        )
+        # create model
+        if project.model is None:
+            model = MLModel.create(
+                project.client,
+                f'{project.name}-model',
+                project=project.uuid
+            )
+        else:
+            model = project.model
 
-        print('uploading model to ml4vision ...')
-
-        remote_model.add_version(
+        print('adding version to project ...')
+        model.add_version(
             os.path.join(config.save_location, 'best_ap_model.pt'),
+            categories=project.categories,
+            architecture="ml4vision-det",
             params = {
                 'min_size': config.transform.min_size if config.transform.resize else False,
                 'pad': 32,
@@ -248,7 +250,7 @@ class Engine:
                 'threshold': float(state["metrics"]["working_point"]["confidence"])
             },
             metrics = {
-                'map_50': round(state["metrics"]["map_50"], 3),
+                'map50': round(state["metrics"]["map_50"], 3),
                 'map': round(state["metrics"]["map"], 3),
                 'precision': round(state["metrics"]["working_point"]["precision"], 3),
                 'recall': round(state["metrics"]["working_point"]["recall"], 3)
